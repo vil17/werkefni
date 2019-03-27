24ec91149a998af2f6b8c331bbab52bc2235a8cf
Villi <vilhjalmur17@ru.is>
2019-03-27 23:23:19 +0000

    eitthvad virkar

--- src/components/BossesContainer/index.js
+++ src/components/BossesContainer/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getBosses } from '../../actions'
+import bossService from '../../services'
+//import PropTypes from 'prop-types'
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getBosses: () => dispatch(getBosses()) 
+        getBosses: () => dispatch(bossService.getBosses()) 
     };
 }
 
 
 class BossContainer extends React.Component {
-    componentDidMount() {
-        const { getBosses } = this.state;
-        var bosses = getBosses;
-        console.log(bosses, 'bosses')
-    }
     constructor(props) { //tharf props?
         super(props);
         this.state = {
-            bosses: []
+            bosses: [],
         }
     }
+    
+    componentWillMount() {
+        const { getBosses } = this.state;
+        fetch("http://localhost:4500/api/bosses").then(resp => { return resp.json() }).then(bossList => this.setState({ bosses: bossList }))
+    }
+    //herna kemur on update i guess
+    
+    
     render() {
-        const { bosses } = this.state;
+        const { bosses } = this.state.bosses.map(boss => ( <div>boss.name</div>));
         console.log(this.state)
         return (
             <div>hi ask</div>
@@ -30,5 +35,7 @@ class BossContainer extends React.Component {
         }
     }
     
+//export default BossContainer
 
-export default connect(null, mapDispatchToProps )(BossContainer);
+var thisstuff = connect(null, mapDispatchToProps)(BossContainer);
+export default thisstuff;
